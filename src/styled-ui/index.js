e30fefa098cb200df46f6793859b980ef8a2e155
cok2exe <cok2exe@gmail.com>
2019-03-17 17:43:50 +0900

    update :) button upgrade and font url

--- src/styled-ui/index.js
+++ src/styled-ui/index.js
@@ -4,5 +4,11 @@
 import styled from 'styled-components'
 
 export const Button = styled.button`
-  background-color: #000000;
+  background-color: ${props =>
+    props.blue ? '#0984e3' : props.red ? '#d63031' : '#6c5ce7'};
+  color: #ffffff;
+  padding: 10px;
+  font-size: 15px;
+  min-width: 60px;
+  border: none;
 `
